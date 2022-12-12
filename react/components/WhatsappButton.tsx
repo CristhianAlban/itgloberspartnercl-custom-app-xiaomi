b82042bc6e26180d5ef9ed5bd625223e9be66e30
cristhian alban <djstylecali@gmail.com>
2022-12-12 09:48:41 -0500

    feat: css handles

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { useCssHandles } from 'vtex.css-handles'
 
 type Props ={
   logo: string
@@ -11,21 +12,26 @@ type Props ={
 
 const WhatsappButton = ({logo, phone, message, width, height}: Props) => {
   const formattedMessage = message.replace(/ /g, "%20");
+  const CSS_HANDLES = ["button__container", "image"]
+  const handles = useCssHandles(CSS_HANDLES)
 
   return <>
     <div className="fixed bottom-2 right-2 flex flexColumn">
-      <a
-        href= {`https://wa.me/${phone}?text=${formattedMessage}`}
-        target= "_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          src={logo}
-          width={width}
-          height={height}
-          alt="Logo de WhatsApp"
-        />
-      </a>
+      <div className={handles.button__container}>
+        <a
+          href= {`https://wa.me/${phone}?text=${formattedMessage}`}
+          target= "_blank"
+          rel="noreferrer noopener"
+        >
+          <img
+            className={handles.image}
+            src={logo}
+            width={width}
+            height={height}
+            alt="Logo de WhatsApp"
+          />
+        </a>
+      </div>
     </div>
   </>
 }
